c6f4ce1bb7940b5a332eef017b96904bfe4cab94
Basit-Technolligence <pakkobra007@gmail.com>
2020-11-15 23:15:10 +0500

    Ad

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "https://testingschoolsystem.herokuapp.com");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 app.get('/',(req,res)=>{
   res.send('SCHOOL MANAGEMENT SYSTEM ADMIN');
