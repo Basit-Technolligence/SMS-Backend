872290f064c230a266c9959cbb20f5bd0028a56d
Basit-Technolligence <pakkobra007@gmail.com>
2020-11-15 23:10:33 +0500

    asd

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var port = process.env.PORT || 5000;
 app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://testingschoolsystem.herokuapp.com");
   res.header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
